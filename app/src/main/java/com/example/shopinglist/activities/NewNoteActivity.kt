2351f93216fd11da493a705871f16ad988b28b9a
Lira <lir0na@mail.ru>
2022-10-26 10:03:15 +0300

    finished creating a notebook

---
+++ app/src/main/java/com/example/shopinglist/activities/NewNoteActivity.kt
@@ -0,0 +1,205 @@
+package com.example.shopinglist.activities
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.graphics.Typeface
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Html
+import android.text.Spannable
+import android.text.style.ForegroundColorSpan
+import android.text.style.StyleSpan
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.core.content.ContextCompat
+import com.example.shopinglist.R
+import com.example.shopinglist.databinding.ActivityNewNoteBinding
+import com.example.shopinglist.entities.NoteItem
+import com.example.shopinglist.fragments.NoteFragment
+import com.example.shopinglist.utils.HtmlManager
+import com.example.shopinglist.utils.MyTouchListener
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.logging.SimpleFormatter
+
+class NewNoteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityNewNoteBinding
+    private var note: NoteItem? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityNewNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        actionBarSettings()
+        getNote()
+        init()
+        onClickColorPicker()
+    }
+
+    private fun onClickColorPicker() = with(binding){
+        imRed.setOnClickListener {
+            setColorForSelectedText(R.color.picker_red)
+        }
+        imBlack.setOnClickListener {
+            setColorForSelectedText(R.color.picker_black)
+        }
+        imBlue.setOnClickListener {
+            setColorForSelectedText(R.color.picker_blue)
+
+        }
+        imGreen.setOnClickListener {
+            setColorForSelectedText(R.color.picker_green)
+
+        }
+        imOrange.setOnClickListener {
+            setColorForSelectedText(R.color.picker_orange)
+
+        }
+        imYellow.setOnClickListener {
+            setColorForSelectedText(R.color.picker_yellow)
+        }
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun init() {
+        binding.colorPicker.setOnTouchListener(MyTouchListener())
+    }
+
+    private fun getNote() {
+        val sNote = intent.getSerializableExtra(NoteFragment.NEW_NOTE_KEY)
+        if (sNote != null) {
+            note = sNote as NoteItem
+            fillNote()
+        }
+    }
+
+    private fun fillNote() = with(binding) {
+        edTitle.setText(note?.title)
+        edDescription.setText(HtmlManager.getFromHtml(note?.content!!).trim())
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.new_note_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.id_save) {
+            setMainResult()
+        } else if (item.itemId == android.R.id.home) {
+            finish()
+        } else if (item.itemId == R.id.id_bolt) {
+            setBoldForSelectedText()
+        } else if (item.itemId == R.id.id_color) {
+            if (binding.colorPicker.isShown) {
+                closeColorPicker()
+            } else {
+                openColorPicker()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setBoldForSelectedText() = with(binding) {
+        val startPos = edDescription.selectionStart
+        val endPos = edDescription.selectionEnd
+
+        val styles = edDescription.text.getSpans(startPos, endPos, StyleSpan::class.java)
+        var boldStyle: StyleSpan? = null
+        if (styles.isNotEmpty()) {
+            edDescription.text.removeSpan(styles[0])
+        } else {
+            boldStyle = StyleSpan(Typeface.BOLD)
+        }
+        edDescription.text.setSpan(boldStyle, startPos, endPos, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        edDescription.text.trim()
+        edDescription.setSelection(startPos)
+    }
+
+    private fun setColorForSelectedText(colorId: Int) = with(binding) {
+        val startPos = edDescription.selectionStart
+        val endPos = edDescription.selectionEnd
+
+        val styles = edDescription.text.getSpans(startPos, endPos, ForegroundColorSpan::class.java)
+        if (styles.isNotEmpty()) edDescription.text.removeSpan(styles[0])
+
+        edDescription.text.setSpan(
+            ForegroundColorSpan(
+                ContextCompat.getColor(this@NewNoteActivity, colorId)
+            ), startPos, endPos, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        edDescription.text.trim()
+        edDescription.setSelection(startPos)
+    }
+
+    private fun setMainResult() {
+        var editState = "new"
+        val tempNote: NoteItem? = if (note == null) {
+            createNewNote()
+        } else {
+            editState = "update"
+            updateNote()
+        }
+        val i = Intent().apply {
+            putExtra(NoteFragment.NEW_NOTE_KEY, tempNote)
+            putExtra(NoteFragment.EDIT_STATE_KEY, editState)
+        }
+        setResult(RESULT_OK, i)
+        finish()
+    }
+
+    private fun updateNote(): NoteItem? = with(binding) {
+        return note?.copy(
+            title = edTitle.text.toString(),
+            content = HtmlManager.toHtml(edDescription.text)
+        )
+    }
+
+    private fun createNewNote(): NoteItem {
+        return NoteItem(
+            null,
+            binding.edTitle.text.toString(),
+            HtmlManager.toHtml(binding.edDescription.text),
+            getCurrentTime(),
+            ""
+        )
+    }
+
+    private fun getCurrentTime(): String {
+        val formatter = SimpleDateFormat("hh:mm:ss - yyyy/MM/dd", Locale.getDefault())
+        return formatter.format(Calendar.getInstance().time)
+    }
+
+    private fun actionBarSettings() {
+        val ab = supportActionBar
+        ab?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    private fun openColorPicker() {
+        binding.colorPicker.visibility = View.VISIBLE
+        val openAnim = AnimationUtils.loadAnimation(this, R.anim.open_color_picker)
+        binding.colorPicker.startAnimation(openAnim)
+    }
+
+    private fun closeColorPicker() {
+        val openAnim = AnimationUtils.loadAnimation(this, R.anim.close_color_picker)
+        openAnim.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(p0: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(p0: Animation?) {
+                binding.colorPicker.visibility = View.GONE
+            }
+
+            override fun onAnimationRepeat(p0: Animation?) {
+
+            }
+
+        })
+        binding.colorPicker.startAnimation(openAnim)
+    }
+}
---
+++ app/src/main/java/com/example/shopinglist/db/MainViewModel.kt
@@ -0,0 +1,32 @@
+package com.example.shopinglist.db
+
+import androidx.lifecycle.*
+import com.example.shopinglist.entities.NoteItem
+import kotlinx.coroutines.launch
+
+class MainViewModel(dataBase: MainDataBase): ViewModel() {
+
+    val dao = dataBase.getDao()
+    val allNotes: LiveData<List<NoteItem>> = dao.getAllNote().asLiveData()
+
+    fun insertNote(note: NoteItem) = viewModelScope.launch {
+        dao.insertNote(note)
+    }
+    fun updateNote (note: NoteItem) = viewModelScope.launch {
+        dao.updateNote(note)
+    }
+
+    fun deleteNote(id: Int) = viewModelScope.launch {
+        dao.deleteNote(id)
+    }
+
+    class MainViewModelFactory(val dataBase: MainDataBase) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(MainViewModel::class.java)){
+                @Suppress("UNCHECKED_CAST")
+                return MainViewModel(dataBase) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModelClass")
+        }
+    }
+}
